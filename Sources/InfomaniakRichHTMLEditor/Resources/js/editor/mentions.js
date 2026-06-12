64361365327a0e9ba49bb9f9e83dbf55dd01f576
Killian Mathias <killian.mathias@infomaniak.com>
2026-06-12 08:31:47 +0200

    feat: Add attribute to tag

--- Sources/InfomaniakRichHTMLEditor/Resources/js/editor/mentions.js
+++ Sources/InfomaniakRichHTMLEditor/Resources/js/editor/mentions.js
@@ -68,6 +68,8 @@ function insertMention(userMail, userName) {
     anchor.setAttribute("data-ik-tag", "");
     anchor.setAttribute("href", `mailto:${userMail}`);
     anchor.setAttribute("contenteditable", "false");
+    anchor.setAttribute("data-not-clickable", "true");
+    anchor.style.pointerEvents = "none";
     anchor.textContent = userName;
 
     const trailingSpace = document.createTextNode(" ");
